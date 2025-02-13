8c243ba4ff19cdd847c1722c04ffaa2c1de9d61e
Hajar-El-Mhassani <hajarelmhassani14@gmail.com>
2025-02-13 13:39:20 +0100

    change my solution in get event day

--- javascript/javascript1/week2/eventApplication.js
+++ javascript/javascript1/week2/eventApplication.js
@@ -7,32 +7,27 @@
 // Function to get the weekday of an event fx today is sunday so the event is on wednesday
 const getEventWeekday = (event) => {    
 
-    // get the current date
     const date = new Date();
-     
-    //get the day of the week form the current date
-    const dayOfWeekNumber =  date.getDay();
-    //array of days
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    //get the day name from the array
-    const dayName= days[dayOfWeekNumber]
-    //if the day name is true print the day name and the event day
-    if (dayName) {
-        console.log('Today is: ' + dayName);  
-        //add the event day to the current day
-        event = dayOfWeekNumber+ event;
-       
-
-    } 
-    //if the event is more than 6 days we use the modulo operator to get the event day
-     if (event>6){
-
-        event = event % days.length;
-       console.log('The event is on: ' + days[event]);
-     
-    }
-   //get the event day from the array
-   return days[event];
+    const todayNumber= date.getDay();
+    const weeksDays=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     
+   if( typeof event !== 'number'){
+        return 'Please enter a number';
+    }
+ if(event === 0){
+        return 'Today is: ' + weeksDays[todayNumber];
+    }
+
+    let eventDay=(todayNumber + event) % weeksDays.length;
+    if(eventDay<0){
+        eventDay += weeksDays.length;
+    }
+
+  return weeksDays[eventDay];
 }
-console.log(getEventWeekday(10));     
+
+    
+
+console.log(getEventWeekday(10));
+console.log(getEventWeekday(0));
+console.log(getEventWeekday(-1));     
