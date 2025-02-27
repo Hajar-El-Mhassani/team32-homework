124b3ebc4ab04a4c09c2e04e0dfeb8b408db7e23
Hajar-El-Mhassani <hajarelmhassani14@gmail.com>
2025-02-27 12:53:31 +0100

    Implement mentor feedback

--- javascript/javascript1/week3/warmup.js
+++ javascript/javascript1/week3/warmup.js
@@ -1,156 +1,135 @@
 // Remove items from array
 const names = [
-    "Peter",
-    "Ahmad",
-    "Yana",
-    "kristina",
-    "Rasmus",
-    "Samuel",
-    "Ahmad",
-    "katrine",
-    "Tala",
-  ];
+  "Peter",
+  "Ahmad",
+  "Yana",
+  "kristina",
+  "Rasmus",
+  "Samuel",
+  "Ahmad",
+  "katrine",
+  "Tala",
+];
 
-  
-  // Write some code here
-   const removeItem = (item)=>{
-   
-    for(let i =names.length -1; i>=0; i--){
-        const name=names[i];
-      
-        if(name===item){
-            names.splice(i,1);
-           
-        }
-       
+// Write some code here
+const removeItem = (item) => {
+  for (let i = names.length - 1; i >= 0; i--) {
+    const name = names[i];
+
+    if (name === item) {
+      names.splice(i, 1);
     }
-    return names;
-  } 
+  }
+  return names;
+};
 
-  const nameToRemove = "Rasmus";
-  
- removeItem(nameToRemove);
+const nameToRemove = "Rasmus";
 
-  console.log(names); 
+removeItem(nameToRemove);
+
+console.log(names);
 
 // When we will be there
- //Fisrt way
+//Fisrt way
 const travelInformation = {
-    speed: 50,
-    destinationDistance: 432,
-    getTimeOfDistination: function(){
-        let travelTime=0;
-        if(this.speed ===0){
-            return `The speed shoud be greater than 0`;
-        }
-        else if(this.destinationDistance===0){
-            return travelTime;
-        }
-        else if(this.speed===0 && this.destinationDistance===0){
-            return travelTime;
-        }
-         
-      else{
+  speed: 50,
+  destinationDistance: 432,
+  getTimeOfDistination: function () {
+    let travelTime = 0;
+    if (this.speed === 0) {
+      return `The speed shoud be greater than 0`;
+    } else if (this.destinationDistance === 0) {
+      return travelTime;
+    } else if (this.speed === 0 && this.destinationDistance === 0) {
+      return travelTime;
+    } else {
+      travelTime = this.destinationDistance / this.speed;
+      // Convert timeTravel to hours and minutes
 
-        travelTime=this.destinationDistance / this.speed;
-     // Convert timeTravel to hours and minutes
-
-     let hours=Math.floor(travelTime);
-     let minutes=Math.floor((travelTime - hours) * 60);
-     return `${hours} hours and ${minutes} minutes`;
-
+      let hours = Math.floor(travelTime);
+      let minutes = Math.floor((travelTime - hours) * 60);
+      return `${hours} hours and ${minutes} minutes`;
     }
+  },
+};
 
-  }
-
-}
-
-const travelTime =travelInformation.getTimeOfDistination();
+const travelTime = travelInformation.getTimeOfDistination();
 console.log(travelTime);
 
 //Second way
- const travelInfo={
-    speed: 50,
-    destinationDistance: 432,
- }
- const getTimeDistination=(obj)=>{
+const travelInfo = {
+  speed: 50,
+  destinationDistance: 432,
+};
+const getTimeDistination = (obj) => {
+  let speed = obj.speed;
+  let distance = obj.destinationDistance;
+  let travelTime = 0;
 
-    let speed=obj.speed;
-    let distance= obj.destinationDistance;
-    let travelTime=0;
+  if (speed === 0) {
+    return `The speed shoud be greater than 0`;
+  } else if (distance === 0) {
+    return travelTime;
+  } else if (speed === 0 && distance === 0) {
+    return travelTime;
+  } else {
+    travelTime = distance / speed;
+    // Convert timeTravel to hours and minutes
 
-if(speed ===0){
-            return `The speed shoud be greater than 0`;
-        }
-        else if(distance===0){
-            return travelTime;
-        }
-        else if(speed===0 && distance===0){
-            return travelTime;
-        }
-         
-      else{
+    let hours = Math.floor(travelTime);
+    let minutes = Math.floor((travelTime - hours) * 60);
+    return `${hours} hours and ${minutes} minutes`;
+  }
+};
 
-        travelTime=distance / speed;
-     // Convert timeTravel to hours and minutes
-        
-     let hours=Math.floor(travelTime);
-     let minutes=Math.floor((travelTime - hours) * 60);
-     return `${hours} hours and ${minutes} minutes`;
-
-    }
- }
-
- const timeTravel= getTimeDistination(travelInfo);
- console.log(timeTravel);
+const timeTravel = getTimeDistination(travelInfo);
+console.log(timeTravel);
 
 //series duration of my life
 
 const seriesDurations = [
-    {
-      title: "Game of thrones",
-      days: 3,
-      hours: 1,
-      minutes: 0,
-    },
-    {
-      title: "Sopranos",
-      days: 3,
-      hours: 14,
-      minutes: 0,
-    },
-    {
-      title: "The Wire",
-      days: 2,
-      hours: 12,
-      minutes: 0,
-    },
-  ];
+  {
+    title: "Game of thrones",
+    days: 3,
+    hours: 1,
+    minutes: 0,
+  },
+  {
+    title: "Sopranos",
+    days: 3,
+    hours: 14,
+    minutes: 0,
+  },
+  {
+    title: "The Wire",
+    days: 2,
+    hours: 12,
+    minutes: 0,
+  },
+];
 
-  const logOutSeriesText=() =>{
-   
-    
-    let totalOfSerie=0;
-    const lifespan=80;
-    for(let i=0; i< seriesDurations.length; i++){
-        const  series= seriesDurations[i];
-         let hours=series.hours/24;
-         let minutes= series.minutes /1440;
+const logOutSeriesText = () => {
+  let totalOfSerie = 0;
+  const lifespan = 80;
+  const DAYS_IN_A_YEAR = 365;
+  for (let i = 0; i < seriesDurations.length; i++) {
+    const series = seriesDurations[i];
+    let hours = series.hours / 24;
+    let minutes = series.minutes / 1440;
 
-         let totalDay=series.days+ hours + minutes;
-         let totalYears= totalDay / 365;
-     
-       let percentageOfLife =Math.floor(((totalYears / lifespan) * 100)*1000)/1000;
-     
-        console.log(`${series.title} took ${percentageOfLife}% of my life`)
-        totalOfSerie += percentageOfLife
-        //rounded with 1 decimal
-        totalOfSerie = Math.ceil(totalOfSerie * 10) / 10;
-      
-    }
-     
-    console.log(`In total is ${totalOfSerie}% of my life`)
+    let totalDay = series.days + hours + minutes;
+    let totalYears = totalDay / DAYS_IN_A_YEAR;
+
+    let percentageOfLife =
+      Math.floor((totalYears / lifespan) * 100 * 1000) / 1000;
+
+    console.log(`${series.title} took ${percentageOfLife}% of my life`);
+    totalOfSerie += percentageOfLife;
+    //rounded with 1 decimal
+    totalOfSerie = Math.ceil(totalOfSerie * 10) / 10;
   }
 
-  logOutSeriesText();
-  
+  console.log(`In total is ${totalOfSerie}% of my life`);
+};
+
+logOutSeriesText();
