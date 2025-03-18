6868cb7405cd039988f8347a0971a6671863aaac
Hajar-El-Mhassani <hajarelmhassani14@gmail.com>
2025-03-18 22:42:30 +0100

    Disabled section

--- javascript/javascript2/week3/app.js
+++ javascript/javascript2/week3/app.js
@@ -1,17 +1,20 @@
 const startbutton = document.querySelector(".startBtn");
 const createbutton = document.querySelector(".createBtn");
+const containerCreate = document.querySelector(".container-create");
+const mainContainer = document.querySelector(".container");
 
+containerCreate.style.display = "none";
 const anotherPage = (path) => {
   window.location.href = path;
 };
 
-startbutton.addEventListener("click", () =>
-  anotherPage("./startQuiz/index.html")
-);
+const showCreateQuiz = () => {
+  mainContainer.style.display = "none"; // Hide the main container
+  containerCreate.style.display = "block"; // Show the create quiz section
+};
+
+startbutton.addEventListener("click", showCreateQuiz);
 
 createbutton.addEventListener("click", () =>
   anotherPage("./createQuiz/index.html")
 );
-
-const containerCreate = document.querySelector(".container-create");
-containerCreate.disabledd = true;
