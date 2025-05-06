8d9d0123b5504a127da551c08ec9095129162085
Hajar-El-Mhassani <hajarelmhassani14@gmail.com>
2025-05-06 14:14:10 +0200

    Modify in social media database

--- databases/week3/social_media/create_tables.sql
+++ databases/week3/social_media/create_tables.sql
@@ -1,57 +1,16 @@
 -- Active: 1746528944152@@127.0.0.1@3307@social_media
+CREATE DATABASE social_media
+    DEFAULT CHARACTER SET = 'utf8mb4';
+USE social_media;
 
-CREATE TABLE IF NOT EXISTS users (
-    id INT  AUTO_INCREMENT,
+-- Users table
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    registration_datetime DATETIME NOT NULL,
-    CONSTRAINT PK_users PRIMARY KEY (id),
-    CONSTRAINT UQ_users UNIQUE (email, name)
-
+    registration_datetime DATETIME NOT NULL
 );
--- posts table
-CREATE TABLE IF NOT EXISTS posts (
-    id INT AUTO_INCREMENT,
-    title text NOT NULL,
-    content TEXT NOT NULL,
-    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    user_id INT NOT NULL,
-    CONSTRAINT PK_posts PRIMARY KEY (id),
-    CONSTRAINT FK_posts_users FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
- -- comments table
-CREATE TABLE IF NOT EXISTS comments (
-    id INT AUTO_INCREMENT,
-    content TEXT NOT NULL,
-    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    post_id INT NOT NULL,
-    user_id INT NOT NULL,
-    parent_comment_id INT DEFAULT NULL,
-    CONSTRAINT PK_comments PRIMARY KEY (id),
-    CONSTRAINT FK_comments_posts FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
-    CONSTRAINT FK_comments_users FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    CONSTRAINT FK_comments_parent_comment FOREIGN KEY (parent_comment_id) REFERENCES comments(id) ON DELETE CASCADE
-
-) ENGINE=InnoDB;
-
---reactions table
-CREATE TABLE IF NOT EXISTS reactions (
-    id INT AUTO_INCREMENT,
-    type ENUM('like', 'highfive','laugh', 'cry', 'angry') NOT NULL,
-    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    post_id INT NOT NULL,
-    user_id INT NOT NULL,
-    comment_id INT DEFAULT NULL,
-    CONSTRAINT PK_reactions PRIMARY KEY (id),
-    CONSTRAINT FK_reactions_posts FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
-    CONSTRAINT FK_reactions_users FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    CONSTRAINT FK_reactions_comments FOREIGN KEY (comment_id) REFERENCES comments(id) ON DELETE CASCADE
-)ENGINE=InnoDB;
-
-
 
 -- Posts table
 CREATE TABLE posts (
@@ -96,7 +55,6 @@ CREATE TABLE reactions (
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (post_id) REFERENCES posts(id),
     FOREIGN KEY (comment_id) REFERENCES comments(id),
-    
     -- Constraints to prevent duplicate reactions
     UNIQUE (user_id, post_id, type_id),
     UNIQUE (user_id, comment_id, type_id)
